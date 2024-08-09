d5df90be03babc091e1089162264a53fb8ea97d9
MNG <guerchaoui7@hotmail.com>
2024-08-09 17:22:32 +0200

    save

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ mongoose.connect(process.env.MONGODB_URI);
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KAI,
+  api_key: process.env.API_KEY,
   api_secret: process.env.API_SECRET,
 });
 
